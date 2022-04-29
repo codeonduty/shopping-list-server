4e58fc24d9f4fae1d164a15eb60cc3f4e2219a1a
vctrblck <sphesihlentshingila@protonmail.com>
2022-04-29 11:17:08 +0200

    Added server initialization functionality

--- server.js
+++ server.js
@@ -4,13 +4,32 @@
 
 // `server.js' is the entry point for modules that define the server.
 
-// - `server.js' initializes the `express' application.
 // - `server.js' initializes the `mongoose' database.
+// - `server.js' initializes the `express' application.
 
 // Libraries:
 
+// None
+
 // Modules:
 
+const config = require('./config');
+const database = require('./mongoose');
+const server = require('./express');
+
 // Code:
+
+// Initialize database
+database.connect(config.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Initialize server
+server.listen(config.PORT, (error) => {
+  if (error) console.log(error);
+
+  console.info('Server initiated on port %s...', config.PORT);
+});
 
 // server.js ends here
