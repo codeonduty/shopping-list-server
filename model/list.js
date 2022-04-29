4b18c77431509fcd01d6a8bb44230b2f37c979ef
vctrblck <sphesihlentshingila@protonmail.com>
2022-04-29 12:03:24 +0200

    Added list model definition

--- model/list.js
+++ model/list.js
@@ -2,8 +2,44 @@
 
 // Libraries:
 
+const mongoose = require('mongoose');
+
 // Modules:
 
+// None
+
 // Code:
+
+const ListSchema = mongoose.Schema(
+  {
+    shopper: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Shopper',
+    },
+
+    listItems: [
+      {
+        item: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Item',
+        },
+
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const List = mongoose.model('List', ListSchema);
+
+module.exports = List;
 
 // list.js ends here
