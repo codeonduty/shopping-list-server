82190ddf4f9b9c00ff519c4f7a76bfd5b141027a
vctrblck <sphesihlentshingila@protonmail.com>
2022-04-29 12:06:06 +0200

    Added item model definition

--- model/item.js
+++ model/item.js
@@ -2,8 +2,49 @@
 
 // Libraries:
 
+const mongoose = require('mongoose');
+
 // Modules:
 
+// None
+
 // Code:
+
+const ItemSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Item name is required!'],
+      unique: [true, 'Item with that name already exists!'],
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    stock: {
+      type: Number,
+      required: [true, 'Item stock quantity is required!'],
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: [true, 'Item price is required!'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Item = mongoose.model('Item', ItemSchema);
+
+module.exports = Item;
 
 // item.js ends here
