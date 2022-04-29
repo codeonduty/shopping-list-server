25d4f3f38f0d5b5223ba9a3f839c9753fcac93b9
vctrblck <sphesihlentshingila@protonmail.com>
2022-04-29 11:12:52 +0200

    Add project-wide configuration

--- config.js
+++ config.js
@@ -10,4 +10,13 @@
 
 // Code:
 
+const config = {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URI:
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/shopping-list',
+  JWT_SECRET: process.env.JWT_SECRET || "mum's the word",
+};
+
+module.exports = config;
+
 // config.js ends here
