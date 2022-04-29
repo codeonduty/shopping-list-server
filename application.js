8d57b760a9c45360851fecddaec3a1ee496fb28f
vctrblck <sphesihlentshingila@protonmail.com>
2022-04-29 11:23:05 +0200

    Added application definition

--- application.js
+++ application.js
@@ -7,8 +7,32 @@
 
 // Libraries:
 
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
 // Modules:
 
+// Routes
+const shopperRoutes = require('./route/shopper');
+const itemRoutes = require('./route/item');
+const listRoutes = require('./route/list');
+
 // Code:
+
+const app = express(); // Instantiate express application
+
+// Pre-routing middleware
+app.use(morgan('dev'));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+// Routing
+app.use('/api', shopperRoutes);
+app.use('/api', itemRoutes);
+app.use('/api', listRoutes);
+
+module.exports = app;
 
 // application.js ends here
